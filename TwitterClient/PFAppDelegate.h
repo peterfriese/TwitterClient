219908cb3e0e0b93717aed25bffa3517d9f8cd47
Peter Friese <peter@peterfriese.de>
2011-09-19 10:07:22 +0200

    Initial Commit

---
+++ TwitterClient/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  TwitterClient
+//
+//  Created by Peter Friese on 19.09.11.
+//  Copyright (c) 2011 itemis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwitterClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterClient
+//
+//  Created by Peter Friese on 19.09.11.
+//  Copyright (c) 2011 itemis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PFAppDelegate class]));
+    }
+}
