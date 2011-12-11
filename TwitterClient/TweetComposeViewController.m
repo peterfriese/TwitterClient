b1ece9ab7885971b89dbbc7d8dd0c8606790b488
Peter Friese <peter@peterfriese.de>
2011-12-11 12:32:58 +0100

    Support all interface orientations.

--- TwitterClient/TweetComposeViewController.m
+++ TwitterClient/TweetComposeViewController.m
@@ -64,8 +64,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 #pragma mark - Actions
